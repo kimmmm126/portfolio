a045bb32a24388d2dc6deb05865bb974e82385e4
kimmmm126 <rlarkgml88@gmail.com>
2024-08-02 17:21:05 +0900

    팝업 오픈 이벤트 추가

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from '../../components/modal';
 import profileImg from '../../assets/images/profile.gif';
 import { InstagramIcon } from '../../constants/svg.js';
 
 const Home = () => {
-  return <Modal title="Portfolio" logo={InstagramIcon} img={profileImg} />;
+  const [openPopup, setOpenPopup] = useState(false);
+
+  const onOpenPopup = () => {
+    setOpenPopup(true);
+  };
+
+  return (
+    <>
+      <button onClick={onOpenPopup}>Open Modal</button>
+      {openPopup && (
+        <Modal title="Portfolio" logo={InstagramIcon} img={profileImg} />
+      )}
+    </>
+  );
 };
 
 export default Home;
